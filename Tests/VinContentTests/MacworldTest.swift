ed53bc45309310b49adf658f6f593378c46a6901
Maurice Parker <mo@vincode.io>
2018-10-19 09:48:52 -0400

    modified wired test and added macworld test

---
+++ Tests/VinContentTests/MacworldTest.swift
@@ -0,0 +1,23 @@
+//
+
+import XCTest
+
+@testable import VinContent
+
+class MacworldTest: VinContentBaseTest {
+    
+    override func setUp() {
+        testHandle = "macworld"
+        expectedTitle = "macOS Mojave and the future of the Mac"
+        expectedPublisher = "Macworld"
+        expectedPublishDate = nil
+        expectedByline = nil
+        expectedDescription = "While one of Mojave\'s signature features involves running iOS apps on the Mac, Dan Moren thinks the latest macOS update proves Apple is still interested in keeping the Mac a tool for power users."
+        expectedSource = "https://www.macworld.com/article/3309580/os-x/article.html"
+    }
+    
+    func testWired() {
+        super.commonTest()
+    }
+    
+}
